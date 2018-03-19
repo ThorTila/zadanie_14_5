345a536e4ecb79bc187847fd742b22a166e2718b
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-03-20 00:56:59 +0100

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "off"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+        "React": false,
+        "ReactDOM": false,
+        "ContactForm": false,
+        "Contacts": false,
+        "Contact": false
+    },
+};
---
+++ script.js
@@ -0,0 +1,3 @@
+/* global App*/
+let app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
